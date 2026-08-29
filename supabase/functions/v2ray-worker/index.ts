8f06fa186b439cbdb553a64163fe4050cb975ab1
ant1380 <erfanmandi1010@gmail.com>
2026-08-29 11:03:39 +0330

    Update index.ts

--- supabase/functions/v2ray-worker/index.ts
+++ supabase/functions/v2ray-worker/index.ts
@@ -46,12 +46,12 @@ const serverGroups = {
     // "vmess://<base64-json>"
   ],
   "2": [
-          "vless://7850157b-2560-435e-9695-c8a76c30f31f@188.114.96.3:443?encryption=none&security=tls&sni=first.corw.ir&fp=random&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=first.corw.ir&path=%2Fvl%2Fhn71UJzifNrP1a3UDm5mTmCS6hX1Gu%3Fed%3D2560#5",
-          "vless://4199303a-8fd4-4e06-8799-7ccad9070671@104.21.74.63:443?encryption=none&security=tls&sni=tesr.erfanhub.ir&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=tesr.erfanhub.ir&path=%2Fvl%2FUoGuCC2ItjAG6iE80ZSx%3Fed%3D2560#6",
-          "vless://c43c59c6-5fdd-4109-8d8d-66578c026f02@104.20.18.167:443?encryption=none&security=tls&sni=IOWzj-QENl8R7zjVMU7klxdCtT2R.wOdiwOW334.WOrkerS.DEV&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&ech=taskulu.com%2Budp%3A%2F%2F1.1.1.1&type=ws&host=iowzj-qenl8r7zjvmu7klxdctt2r.wodiwow334.workers.dev&path=%2Fvl%2FCvYgbOvrFqG4ihZhsExQ%3Fed%3D2560#7",
-          "vless://02b1ea62-173d-43df-a566-6f0f65536e23@45.67.215.67:443?encryption=none&security=tls&sni=hola.erfanfamily.ir&fp=chrome&ech=cloudflare-ech.com%2Bhttps%3A%2F%2F8.8.8.8%2Fdns-query&type=ws&host=hola.erfanfamily.ir&path=%2F%3Fed%3D2048#8",
-          "vless://57ed544c-9260-43e8-8354-f626bd44f58b@69.46.46.105:443?encryption=none&security=tls&sni=welldone-node-production.up.railway.app&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1&type=ws&host=welldone-node-production.up.railway.app&path=%2Fvless#%F0%9F%87%BA%F0%9F%87%B8%20United%20States%20East%202",
-         "vless://67b30697-b51e-4ae6-a182-161e4a05e8a0@69.46.46.28:443?encryption=none&security=tls&sni=mediumrare-node-production.up.railway.app&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1&type=ws&host=mediumrare-node-production.up.railway.app&path=%2Fvless#%F0%9F%87%B3%F0%9F%87%B1%20The%20Netherlands%203",
+          "vless://7850157b-2560-435e-9695-c8a76c30f31f@188.114.96.3:443?encryption=none&security=tls&sni=first.corw.ir&fp=random&alpn=http%2F1.1&type=ws&host=first.corw.ir&path=%2Fvl%2Fhn71UJzifNrP1a3UDm5mTmCS6hX1Gu%3Fed%3D2560#5",
+        "vless://4199303a-8fd4-4e06-8799-7ccad9070671@104.21.74.63:443?encryption=none&security=tls&sni=tesr.erfanhub.ir&fp=random&alpn=http%2F1.1&type=ws&host=tesr.erfanhub.ir&path=%2Fvl%2FUoGuCC2ItjAG6iE80ZSx%3Fed%3D2560#6",
+        "vless://c43c59c6-5fdd-4109-8d8d-66578c026f02@104.20.18.167:443?encryption=none&security=tls&sni=IOWzj-QENl8R7zjVMU7klxdCtT2R.wOdiwOW334.WOrkerS.DEV&fp=random&alpn=http%2F1.1&type=ws&host=iowzj-qenl8r7zjvmu7klxdctt2r.wodiwow334.workers.dev&path=%2Fvl%2FCvYgbOvrFqG4ihZhsExQ%3Fed%3D2560#7",
+        "vless://02b1ea62-173d-43df-a566-6f0f65536e23@172.67.163.166:443?encryption=none&security=tls&sni=hola.erfanfamily.ir&fp=random&type=ws&host=hola.erfanfamily.ir&path=%2F%3Fed%3D2048#8",
+        "vless://57ed544c-9260-43e8-8354-f626bd44f58b@188.114.96.3:443?encryption=none&security=tls&sni=joke.corw.ir&fp=random&alpn=h3%2Ch2%2Chttp%2F1.1&type=ws&host=joke.corw.ir&path=welldone-node-production.up.railway.app%3A443%2Fvless#%F0%9F%87%BA%F0%9F%87%B8%20United%20States%20East%202",
+        "vless://67b30697-b51e-4ae6-a182-161e4a05e8a0@188.114.96.1:443?encryption=none&security=tls&sni=yes.docom47457.workers.dev&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1&type=ws&host=yes.docom47457.workers.dev&path=mediumrare-node-production.up.railway.app%3A443%2Fvless#%F0%9F%87%B3%F0%9F%87%B1%20The%20Netherlands%203",
   ],
   "3": [
         "vless://14e28b36-9a4d-40a4-9aff-fb63163c74f4@172.64.229.36:443?encryption=none&security=tls&sni=test1200.erfanhub.ir&fp=random&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=test1200.erfanhub.ir&path=%2Fpyip%3D178.156.139.174#8",
