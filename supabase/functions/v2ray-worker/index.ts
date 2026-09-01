e0591e8a97e339ab95d82f7ce39de381bfbadc59
ant1380 <erfanmandi1010@gmail.com>
2026-09-01 22:29:09 +0330

    Update index.ts

--- supabase/functions/v2ray-worker/index.ts
+++ supabase/functions/v2ray-worker/index.ts
@@ -38,12 +38,11 @@ const serverGroups = {
           "vless://9facaeda-08cb-4032-98ef-208c65c18027@104.18.152.249:443?encryption=none&security=tls&sni=urmy.erfanfamily2.ir&fp=chrome&alpn=http%2F1.1&type=ws&host=urmy.erfanfamily2.ir&path=%2Fvl%2F0LqYf21RNpzenuMzsU8d1JDVi%3Fed%3D2560#2",
           "vless://f1ebace6-6bf9-4fe8-8a42-5848975f52ea@104.18.152.249:443?encryption=none&security=tls&sni=fO9lJCOvpe4B394uDeO4g.pAGes.DEV&fp=chrome&alpn=http%2F1.1&ech=taskulu.com%2Budp%3A%2F%2F1.1.1.1&type=ws&host=fo9ljcovpe4b394udeo4g.pages.dev&path=%2Fvl%2FgaHIeQrRumXAaZfvi7NDb9sy%3Fed%3D2560#3",
           "vless://292032c7-15a3-4eaf-8d76-076c13832278@172.67.184.127:443?encryption=none&security=tls&sni=testu.erfanfamily.ir&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=testu.erfanfamily.ir&path=%2Fvl%2FKIG6R8zHJxjnEPNXIeF7YLPOG9oN4%3Fed%3D2560#4",
-          "vless://09164f6e-ff7b-4ced-b4f2-0007e19d33ad@66.33.22.220:50955?encryption=none&security=reality&sni=intel.com&fp=chrome&pbk=e4ykUF7SzqO0e8xowkPq5vdbgoRnpCSjNwkN9fY54HI&sid=da99d8d99d&spx=%2F6c8dda47856038a&type=tcp&headerType=none#%F0%9F%87%B3%F0%9F%87%B1%20The%20Netherlands",
           "vless://1e08ec7a-d612-4cfd-8860-b9a4210eb34a@66.33.22.223:57780?encryption=none&security=reality&sni=www.speedtest.net&fp=chrome&pbk=MZk1twtuzERF0hokztu27vRf8MgmELXkKrsrAQY1dTs&sid=a953f377ab00e115&spx=%2F9b6785e2423c139&type=tcp&headerType=none#%F0%9F%87%BA%F0%9F%87%B8%20United%20States%20West",
           "vless://4f3b5cbd-cad2-408e-b017-7b23ad179edd@69.46.46.7:443?encryption=none&security=tls&sni=lola-node-production.up.railway.app&fp=random&alpn=h2&type=ws&host=lola-node-production.up.railway.app&path=%2Fvless#%F0%9F%87%B3%F0%9F%87%B1%20The%20Netherlands%202",
-          "vless://d357fb6a-1a32-4a7e-8a1b-fb2fccdaf171@66.33.22.220:35903?encryption=none&security=none&type=tcp&headerType=none#raw%20railway",
           "vless://e632d034-b3ce-41fb-ae5e-719f2cc6fb3e@69.46.46.4:443?encryption=none&security=tls&sni=farzsaz-node-production.up.railway.app&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1&type=ws&host=farzsaz-node-production.up.railway.app&path=%2Fvless#%F0%9F%87%BA%F0%9F%87%B8%20United%20States%20East",
           "vless://7812d9fb-ee0f-4e5f-b346-f1d596201a19@69.46.46.18:443?encryption=none&security=tls&sni=sohrabs.up.railway.app&fp=chrome&alpn=h3%2Ch2%2Chttp%2F1.1&type=ws&host=sohrabs.up.railway.app&path=%2Frefuse#%F0%9F%87%BA%F0%9F%87%B8%20United%20States%20East%202",
+          "vless://256bfe46-a5f0-11f1-a65c-ef77b3ab429e@188.114.96.1:443?encryption=none&security=tls&sni=yes.docom47457.workers.dev&fp=chrome&alpn=http%2F1.1&type=ws&host=yes.docom47457.workers.dev&path=%2Ftr2.vpnjantit.com%3A10002%2Fvpnjantit#%F0%9F%87%B9%F0%9F%87%B7%20T%C3%BCrkiye",
     // "vmess://<base64-json>"
   ],
   "2": [
